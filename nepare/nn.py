bc2f5fd8ef0838c00e59edbee0c7f21bb0ee7516
JacksonBurns <jwburns@mit.edu>
2025-02-15 00:28:17 -0500

    model - add multitask, fix batch_size warning for custom inputs

--- nepare/nn.py
+++ nepare/nn.py
@@ -4,13 +4,13 @@ import torch
 import lightning
 
 class FeedforwardNeuralNetwork(lightning.LightningModule):
-    def __init__(self, input_size: int, hidden_size: int, num_layers: int, activation: torch.nn.Module = torch.nn.ReLU, lr: float = 1e-3):
+    def __init__(self, input_size: int, hidden_size: int, num_layers: int, activation: torch.nn.Module = torch.nn.ReLU, lr: float = 1e-3, n_targets: int = 1):
         super().__init__()
         _modules = OrderedDict()
         for i in range(num_layers):
             _modules[f"hidden_{i}"] = torch.nn.Linear(input_size if i == 0 else hidden_size, hidden_size)
             _modules[f"{activation.__name__.lower()}_{i}"] = activation()
-        _modules["readout"] = torch.nn.Linear(hidden_size, 1)
+        _modules["readout"] = torch.nn.Linear(hidden_size, n_targets)
         self.fnn = torch.nn.Sequential(_modules)
         self.lr = lr
         self.save_hyperparameters()
@@ -41,8 +41,8 @@ class FeedforwardNeuralNetwork(lightning.LightningModule):
         return self(X[0])
 
 class NeuralPairwiseRegressor(FeedforwardNeuralNetwork):
-    def __init__(self, input_size, hidden_size, num_layers, activation = torch.nn.ReLU, lr: float = 1e-4):
-        super().__init__(2*input_size, hidden_size, num_layers, activation, lr)
+    def __init__(self, input_size, hidden_size, num_layers, activation = torch.nn.ReLU, lr: float = 1e-4, n_targets: int = 1):
+        super().__init__(2*input_size, hidden_size, num_layers, activation, lr, n_targets)
 
     def _step(self, batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor], name: str):
         x_1, x_2, y = batch
@@ -55,8 +55,8 @@ class NeuralPairwiseRegressor(FeedforwardNeuralNetwork):
         return self(x)
 
 class LearnedEmbeddingNeuralPairwiseRegressor(NeuralPairwiseRegressor):
-    def __init__(self, embedding_module: torch.nn.Module, embedding_size, hidden_size, num_layers, activation=torch.nn.ReLU, lr: float = 1e-4):
-        super().__init__(embedding_size, hidden_size, num_layers, activation, lr)
+    def __init__(self, embedding_module: torch.nn.Module, embedding_size, hidden_size, num_layers, activation=torch.nn.ReLU, lr: float = 1e-4, n_targets: int = 1):
+        super().__init__(embedding_size, hidden_size, num_layers, activation, lr, n_targets)
         # must be a learnable module that takes two inputs of some arbitrary type and generates a vector representation
         self.embedding_module = embedding_module
         self.save_hyperparameters(ignore=['embedding_module'])
@@ -71,7 +71,7 @@ class LearnedEmbeddingNeuralPairwiseRegressor(NeuralPairwiseRegressor):
         x_1, x_2, y = batch
         y_hat = self((x_1, x_2))
         loss = torch.nn.functional.mse_loss(y_hat, y)
-        self.log(f"{name}/loss", loss, prog_bar=True)
+        self.log(f"{name}/loss", loss, prog_bar=True, batch_size=y.shape[0])  # first object in batch can be hard to infer batch size
         return loss
 
     def predict_step(self, batch: tuple[object, object]):
