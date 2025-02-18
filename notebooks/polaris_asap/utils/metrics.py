ad52eb8d814f8b710f298a22d9e98047dfb133e0
JacksonBurns <jwburns@mit.edu>
2025-02-18 08:09:09 -0500

    complete run for admet with mordred embedding

--- notebooks/polaris_asap/utils/metrics.py
+++ notebooks/polaris_asap/utils/metrics.py
@@ -2,4 +2,4 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from scipy.stats import pearsonr
 
 def evaluate_predictions(y_true, y_pred):
-    return {"Pearson r": pearsonr(y_true, y_pred), "MSE": mean_squared_error(y_true, y_pred), "MAE": mean_absolute_error(y_true, y_pred)}
+    return {"Pearson r": pearsonr(y_true, y_pred).correlation, "MSE": mean_squared_error(y_true, y_pred), "MAE": mean_absolute_error(y_true, y_pred)}
